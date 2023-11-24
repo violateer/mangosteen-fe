f39875dd7200e9316a2266e65ad0eac09350321e
violateer <1828257089@qq.com>
2023-11-24 22:18:17 +0800

    css初始化

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,11 @@
 import { defineComponent, ref } from "vue";
+import "./App.scss";
 
 export const App = defineComponent({
   setup() {
     return () => (
       <>
-        <header>
-          导航
-          <ul>
-            <li>
-              <router-link to="/">Foo</router-link>
-            </li>
-            <li>
-              <router-link to="/about">Bar</router-link>
-            </li>
-          </ul>
-        </header>
-        <div>
-          <router-view></router-view>
-        </div>
-        <footer>页脚</footer>
+        <router-view></router-view>
       </>
     );
   },
