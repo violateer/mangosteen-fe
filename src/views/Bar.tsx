806b5b7b54c7bdd7fc0c619ca14c4e7e8381cc5c
violateer <1828257089@qq.com>
2023-11-25 13:01:51 +0800

    修改默认路由

--- src/views/Bar.tsx
+++
@@ -1,7 +0,0 @@
-import { defineComponent, ref } from "vue";
-
-export const Bar = defineComponent({
-  setup(props, context) {
-    return () => <>Bar</>;
-  },
-});
--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,9 @@ import vue from "@vitejs/plugin-vue";
 import vueJsx from "@vitejs/plugin-vue-jsx";
 
 export default defineConfig({
+  server: {
+    port: 3000,
+  },
   plugins: [
     vue(),
     vueJsx({
