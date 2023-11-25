4b9254b7761ac9dd438d500542e8dbcd36f01a05
violateer <1828257089@qq.com>
2023-11-25 15:22:46 +0800

    4个欢迎页面

--- src/components/welcome/First.tsx
+++ src/components/welcome/First.tsx
@@ -8,7 +8,7 @@ export const First = defineComponent({
     return () => (
       <div class={s.wrapper}>
         <div class={s.card}>
-          <img src={pig} />
+          <img class={s.icon} src={pig} />
           <h2>
             会挣钱
             <br />
--- src/components/welcome/Third.tsx
+++ src/components/welcome/Third.tsx
@@ -1,7 +1,28 @@
 import { defineComponent, ref } from "vue";
+import s from "./First.module.scss";
+import clock from "../../assets/icons/clock.svg";
+import { RouterLink } from "vue-router";
 
 export const Third = defineComponent({
   setup(props, context) {
-    return () => <>Third</>;
+    return () => (
+      <div class={s.wrapper}>
+        <div class={s.card}>
+          <img class={s.icon} src={clock} />
+          <h2>
+            每日提醒
+            <br />
+            不遗漏每一笔账单
+          </h2>
+        </div>
+        <div class={s.actions}>
+          <RouterLink class={s.fake} to="/start">
+            跳过
+          </RouterLink>
+          <RouterLink to="/welcome/4">下一页</RouterLink>
+          <RouterLink to="/start">跳过</RouterLink>
+        </div>
+      </div>
+    );
   },
 });
