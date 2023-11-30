70ae7e6030fdeddfb7d6d899f0b11dceff071541
violateer <1828257089@qq.com>
2023-11-30 21:55:42 +0800

    完成数字键盘和日期选择

--- src/components/Item/ItemCreate.tsx
+++ src/components/Item/ItemCreate.tsx
@@ -1,12 +1,21 @@
-import { PropType, defineComponent, ref } from "vue";
+import { PropType, defineComponent, reactive, ref } from "vue";
 import s from "./ItemCreate.module.scss";
 import { MainLayout } from "../../layouts/MainLayout";
 import { Icon } from "../../shared/Icon";
 import { Tab, Tabs } from "../../shared/Tabs";
+import { InputPad } from "./InputPad";
 
 export const ItemCreate = defineComponent({
   setup(props, context) {
     const kind = ref("支出");
+    const formData = reactive<Partial<Item>>({
+      kind: "expenses",
+      tag_ids: [],
+      amount: 0,
+      happen_at: new Date().toISOString(),
+    });
+
+    const onSubmit = () => {};
     return () => (
       <div class={s.itemPage}>
         <MainLayout>
@@ -22,6 +31,14 @@ export const ItemCreate = defineComponent({
                   <Tab name="支出">icon 列表</Tab>
                   <Tab name="收入">icon 列表2</Tab>
                 </Tabs>
+
+                <div class={s.inputPad_wrapper}>
+                  <InputPad
+                    v-model:happenAt={formData.happen_at}
+                    v-model:amount={formData.amount}
+                    onSubmit={onSubmit}
+                  />
+                </div>
               </>
             ),
           }}
--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,8 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import vueJsx from "@vitejs/plugin-vue-jsx";
+import Components from 'unplugin-vue-components/vite';
+import { VantResolver } from '@vant/auto-import-resolver';
 
 // @ts-nocheck
 import { svgstore } from "./src/vite_plugins/svgstore";
@@ -16,5 +18,8 @@ export default defineConfig({
       mergeProps: true,
     }),
     svgstore(),
+    Components({
+      resolvers: [VantResolver()],
+    }),
   ],
 });
