78e294ca925fab6d1e7f98753f844e90160c44ae
violateer <1828257089@qq.com>
2023-12-05 22:03:26 +0800

    修复vant变量样式覆盖

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,10 @@
 import { createApp } from "vue";
-import { App } from "./App";
 import { createRouter } from "vue-router";
 import { routes } from "./config/routes";
 import { history } from "./shared/history";
 import "@svgstore";
 import "vant/lib/index.css";
+import { App } from "./App"; // 这行放最后，因为需要vars.scss的变量覆盖vant变量
 
 const router = createRouter({ history, routes });
 
