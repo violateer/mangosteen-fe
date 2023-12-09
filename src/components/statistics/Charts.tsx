6283f6f74eaed94bef9063889a75f9e398619408
violateer <1828257089@qq.com>
2023-12-09 11:54:39 +0800

    添加饼图

--- src/components/statistics/Charts.tsx
+++ src/components/statistics/Charts.tsx
@@ -17,6 +17,7 @@ export const Charts = defineComponent({
   setup(props, context) {
     const category = ref("expenses");
     const refDiv = ref<HTMLDivElement>();
+    const refDiv2 = ref<HTMLDivElement>();
     onMounted(() => {
       if (refDiv.value === undefined) return;
       // 基于准备好的dom，初始化echarts实例
@@ -50,6 +51,49 @@ export const Charts = defineComponent({
       // 使用刚指定的配置项和数据显示图表。
       myChart.setOption(option);
     });
+
+    onMounted(() => {
+      if (refDiv2.value === undefined) return;
+      // 基于准备好的dom，初始化echarts实例
+      var myChart = echarts.init(refDiv2.value);
+
+      // 指定图表的配置项和数据
+      var option: echarts.EChartOption = {
+        grid: [
+          {
+            left: 0,
+            top: 0,
+            right: 0,
+            bottom: 20,
+          },
+        ],
+        series: [
+          {
+            name: "Access From",
+            type: "pie",
+            radius: "50%",
+            data: [
+              { value: 1048, name: "Search Engine" },
+              { value: 735, name: "Direct" },
+              { value: 580, name: "Email" },
+              { value: 484, name: "Union Ads" },
+              { value: 300, name: "Video Ads" },
+            ],
+            emphasis: {
+              itemStyle: {
+                shadowBlur: 10,
+                shadowOffsetX: 0,
+                shadowColor: "rgba(0, 0, 0, 0.5)",
+              },
+            },
+          },
+        ],
+      };
+
+      // 使用刚指定的配置项和数据显示图表。
+      myChart.setOption(option);
+    });
+
     return () => (
       <div class={s.wrapper}>
         <FormItem
@@ -62,6 +106,7 @@ export const Charts = defineComponent({
           v-model={category.value}
         ></FormItem>
         <div ref={refDiv} class={s.demo}></div>
+        <div ref={refDiv2} class={s.demo2}></div>
       </div>
     );
   },
